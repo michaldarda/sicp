6cc15d5fce53cbaf6d92b256403ccde953ead6e7
Michał Darda <mdarda@pm.me>
2019-08-30 22:33:57 +0200

    Factorial iterative

--- 12.rkt
+++ 12.rkt
@@ -1,14 +1,22 @@
 #lang sicp
 
-(define (assert condition)
-  (if (not condition)
-      (error "Does not match condition")))
+(define (assert-eql actual expected)
+  (if (not (= expected actual))
+      (error "Does not match condition, expected: " expected " actual: " actual)))
 
 (define (factorial n)
   (if (= n 0) 1
       (* n (factorial (- n 1)))))
 
-(assert (= (factorial 5) 120))
+(assert-eql (factorial 5) 120)
+
+(define (factorial-iter n)
+  (define (factorial-iter-inner product counter)
+    (if (> counter n) product
+        (factorial-iter-inner (* counter product) (+ counter 1))))
+  (factorial-iter-inner 1 1))
+
+(assert-eql (factorial-iter 5) 120)
 
 (define (fib n)
   (define (fib-iter a b c)
@@ -16,7 +24,7 @@
           (else (fib-iter (+ a b) a (dec c)))))
   (fib-iter 1 0 n))
 
-(assert (= (fib 1) 1))
-(assert (= (fib 2) 2))
-(assert (= (fib 5) 8))
-(assert (= (fib 20) 10946))
+(assert-eql (fib 1) 1)
+(assert-eql (fib 2) 2)
+(assert-eql (fib 5) 8)
+(assert-eql (fib 20) 10946)
