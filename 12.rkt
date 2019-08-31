a21aeb8f3d7293f1c178fc175da7396431c60a8f
Michał Darda <mdarda@pm.me>
2019-08-31 21:53:07 +0200

    fast mul iter

--- 12.rkt
+++ 12.rkt
@@ -130,17 +130,22 @@
 
 ;; 1.18
 
+;; b^(n/2)^2 = (b^2)^n/2
+
+;; a * b = 2(1/2* a * 1/2b)
+
 (define (mul-log-iter a b)
   (define (double x) (+ x x))
   (define (halve x) (/ x 2))
 
-  (define (mul-log-inner b accu)
+  (define (mul-log-inner a b accu)
     (cond [(= b 0) accu]
-          ;; [(even? b) (mul-log-inner (halve b) (double (+ accu a)))]
-          [else (mul-log-inner (- b 1) (+ accu a))]))
+          [(even? b) (mul-log-inner (double a) (halve b) accu)]
+          [else (mul-log-inner a (- b 1) (+ accu a))]))
 
-  (mul-log-inner b 0))
+  (mul-log-inner a b 0))
 
+(assert-eql (mul-log-iter 1 0) 0)
 (assert-eql (mul-log-iter 1 2) 2)
 (assert-eql (mul-log-iter 2 2) 4)
 (assert-eql (mul-log-iter 36 36) 1296)
