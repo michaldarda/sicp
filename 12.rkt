6f6941ff2a74214e2a47dd4bd871f127a2f83aed
Michał Darda <mdarda@pm.me>
2019-08-31 23:09:40 +0200

    damn that was hard

--- 12.rkt
+++ 12.rkt
@@ -149,3 +149,24 @@
 (assert-eql (mul-log-iter 1 2) 2)
 (assert-eql (mul-log-iter 2 2) 4)
 (assert-eql (mul-log-iter 36 36) 1296)
+
+;; 1.19
+
+
+(define (fib-log n)
+  (define (square x) (* x x))
+
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (square p) (square q))
+                     (+ (square q) (* 2 p q))
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
