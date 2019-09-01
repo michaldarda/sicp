a1e4af6f726217fe3bb015db90fd209c69c1e90c
Michał Darda <mdarda@pm.me>
2019-09-01 09:16:33 +0200

    Rewrite example for primalty testign

--- 12.rkt
+++ 12.rkt
@@ -178,3 +178,19 @@
   (if (= b 0)
       a
       (gcd b (remainder a b))))
+
+;; Example: Testing for primality
+
+(define (square x) (* x x))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
