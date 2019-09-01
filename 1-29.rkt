d916db29afc7e29b0c617f0dd5c33df9e1bbf7f8
Michał Darda <mdarda@pm.me>
2019-09-02 01:11:17 +0200

    Simson

---
+++ 1-29.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (cube x) (* x x x))
+
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+
+  (define (loop n accu b)
+    (if (= n 0)
+        (+ accu (f a))
+        (loop (- n 1)
+              (if (even? n)
+                  (+ accu (* 2 (f (- b h))))
+                  (+ accu (* 4 (f (- b h)))))
+              (- b h))))
+
+  (* (/ h 3) (+ (f a) (loop n (f b) b))))
+
+
+(simpson cube 0 1 10)
+(simpson cube 0 1 100)
