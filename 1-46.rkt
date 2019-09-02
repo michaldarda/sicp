d1abeb8f16854fb3de57ceacbf87451cd5d805ee
Michał Darda <mdarda@pm.me>
2019-09-02 23:51:52 +0200

    1-46

---
+++ 1-46.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+
+(define (iterative-improve good-enough? improve)
+  (define (iterative-improver guess)
+    (if (good-enough? guess)
+        guess
+        (iterative-improver (improve guess))))
+  iterative-improver)
+
+
+(define (sqrt x)
+  (define (improve guess)
+    (average guess (/ x guess)))
+
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+
+  (define (sqrt-iter guess x)
+    ((iterative-improve good-enough? improve) guess))
+  (sqrt-iter 1.0 x))
+
+(sqrt 25)
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? guess)
+    (< (abs (- guess (f guess)))
+       tolerance))
+  ((iterative-improve
+    close-enough?
+    (lambda (guess) (f guess)))
+   first-guess))
+
+(fixed-point cos 1.0)
