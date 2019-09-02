b0aeb0e4b67f1d1f6b1660d47f2692be5f9527a0
Michał Darda <mdarda@pm.me>
2019-09-03 00:43:01 +0200

    use let*

--- 2-1.rkt
+++ 2-1.rkt
@@ -11,8 +11,8 @@
           [(and (negative? n) (negative? d)) 1]
           [(and (positive? n) (negative? d)) (- 1)]
           [(and (negative? n) (positive? d)) (- 1)]))
-  (let ([g (abs (gcd n d))]
-        [sign (sign n d)]
-        [n (abs n)]
-        [d (abs d)])
-    (cons (* sign (/ n g)) (/ d g))))
+  (let* ([sign (sign n d)]
+         [g (gcd n d)]
+         [n (abs (/ n g))]
+         [d (abs (/ d g))])
+    (cons (* sign n) d)))
