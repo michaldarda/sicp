366855a3b0cc8d3e3dbe65483386afe1acf92e0d
Michał Darda <mdarda@pm.me>
2019-09-08 22:04:46 +0200

    2-08

---
+++ 2-08.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval) (car interval))
+
+(define (upper-bound interval) (cdr interval))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
