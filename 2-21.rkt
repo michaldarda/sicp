d8a0398c80c52092c3095817ad9e7a1c2a626f29
Michał Darda <mdarda@pm.me>
2019-09-11 23:25:37 +0200

    2-21

--- 2-21.rkt
+++ 2-21.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
 (define (square-list xs)
-  (map (lambda (x) (* x x)) xs))
+  (if (null? xs)
+      null
+      (cons ((lambda (x) (* x x)) (car xs))
+            (square-list (cdr xs)))))
 
 (square-list (list 1 2 3 4 5))
+
+(define (square-list-map xs)
+  (map (lambda (x) (* x x)) xs))
+
+(square-list-map (list 1 2 3 4 5))
