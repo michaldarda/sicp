146d54dc13b8ee30d8fa3aab81594fae42ffb4a0
Michał Darda <mdarda@pm.me>
2019-09-12 22:55:02 +0200

    2-29

--- 2-29.rkt
+++ 2-29.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(define not-pair? (negate pair?))
+
 (define (make-mobile left right)
   (list left right))
 
@@ -19,10 +21,32 @@
   (cadr br))
 
 (define (total-weight bm)
-  (cond [(not (pair? (right-branch bm))) (right-branch bm)]
-        [(not (pair? (left-branch bm))) (total-weight (right-branch bm))]
+  (cond [(not-pair? bm) bm]
+        [(not-pair? (right-branch bm)) (right-branch bm)]
+        [(not-pair? (left-branch bm)) (total-weight (right-branch bm))]
         [else (+ (total-weight (left-branch bm))
                  (total-weight (right-branch bm)))]))
 
 (define x (make-mobile (make-branch 4 10) (make-branch 4 10)))
 (define y (make-mobile (make-branch 4 10) x))
+
+(total-weight x)
+(total-weight y)
+
+(define z (make-mobile (make-branch 2 3) (make-branch 2 3)))
+(total-weight z)
+
+(define (balanced? bm)
+  (define (torque b)
+    (* (branch-length b) (total-weight (branch-structure b))))
+
+  (if (not-pair? bm)
+      true
+      (and (= (torque (left-branch bm))
+              (torque (right-branch bm)))
+           (balanced? (branch-structure (left-branch bm)))
+           (balanced? (branch-structure (right-branch bm))))))
+
+(balanced? x)
+(define d (make-mobile (make-branch 10 z) (make-branch 12 5)))
+(balanced? d)
