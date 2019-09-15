57ee3cee0a8fa3c1737ca23f338b7cfac4b367a9
Michał Darda <mdarda@pm.me>
2019-09-15 22:19:55 +0200

    2-32

---
+++ 2-32.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (subsets s)
+  (if (null? s)
+      (list null)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+
+(subsets '(1 2 3))
