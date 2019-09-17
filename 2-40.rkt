2842f4a09d50ae446e98860b2c1f18c5c82d676c
Michał Darda <mdarda@pm.me>
2019-09-18 00:20:50 +0200

    2-40

---
+++ 2-40.rkt
@@ -0,0 +1,57 @@
+#lang racket
+
+(define (inc x) (+ 1 x))
+(define (square x) (* x x))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define n 10)
+
+(accumulate append null (map (lambda (i)
+                               (map (lambda (j) (list i j))
+                                    (enumerate-interval 1 (- i 1))))
+                               (enumerate-interval 1 n)))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(unique-pairs n)
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs n)
