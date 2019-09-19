f07d88d1370e0a8c1d764f3343ff9b35d6b21e50
Michał Darda <mdarda@pm.me>
2019-09-19 23:53:32 +0200

    2-42 WIP

---
+++ 2-42.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+;;
+
+(define (empty-board) (list (list)))
+(define (safe? k positions) #t)
+(define (adjoin-position new-row k rest-of-queens) rest-of-queens)
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
