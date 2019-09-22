8feb9dc1173874fa280b29829465742af2ff8bc7
Michał Darda <mdarda@pm.me>
2019-09-22 22:01:31 +0200

    2-42

--- 2-42.rkt
+++ 2-42.rkt
@@ -16,10 +16,48 @@
 
 ;;
 
+(define (make-queen row col)
+  (cons row col))
+
+(define (row-queen qc)
+  (car qc))
+
+(define (col-queen qc)
+  (cdr qc))
+
+(define (same-queen? a b)
+  (and (= (row-queen a) (row-queen b))
+       (= (col-queen a) (col-queen b))))
+
+(define (in-check? a b)
+  "Checks if queens a b are in check"
+  (define (same-row? a b)
+    (= (row-queen a) (row-queen b)))
+  (define (same-col? a b)
+    (= (col-queen a) (col-queen b)))
+  (define (same-diagonal? a b)
+    (let ([xa (row-queen a)]
+          [xb (row-queen b)]
+          [ya (col-queen a)]
+          [yb (col-queen b)])
+      (= (abs (- xa xb))
+         (abs (- ya yb)))))
+  (or (same-row? a b)
+      (same-col? a b)
+      (same-diagonal? a b)))
+
 (define empty-board null)
-(define (safe? k positions) #f)
+(define (safe? k positions)
+  (letrec ([queen (list-ref positions (- k 1))]
+           [queens-that-are-in-check
+            (filter (lambda (q)
+                      (and (not (same-queen? queen q))
+                           (in-check? queen q)))
+                    positions)])
+    (empty? queens-that-are-in-check)))
+
 (define (adjoin-position new-row k rest-of-queens)
-  (append rest-of-queens (list (cons new-row k))))
+  (append rest-of-queens (list (make-queen new-row k))))
 
 (define (queens board-size)
   (define (queen-cols k)
@@ -36,4 +74,6 @@
           (queen-cols (- k 1))))))
   (queen-cols board-size))
 
-(queens 10)
+(queens 1)
+(queens 2)
+(queens 6)
