90bb1d68670ad46a533d446fc2e85f9f28f46a42
Michał Darda <mdarda@pm.me>
2019-09-23 15:58:35 +0200

    2-50

--- 2-50.rkt
+++ 2-50.rkt
@@ -13,6 +13,27 @@
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
 
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
 (paint einstein)
 (paint (flip-vert einstein))
 (paint (flip-horiz einstein))
+(paint (rotate90 einstein))
+(paint (rotate180 einstein))
+(paint (rotate270 einstein))
