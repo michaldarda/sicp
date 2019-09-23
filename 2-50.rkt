2d7209f09bfc5a97dcb8ecfca8a5a6899f922eb6
Michał Darda <mdarda@pm.me>
2019-09-23 15:35:51 +0200

    2-50

---
+++ 2-50.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(#%require sicp-pict)
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint einstein)
+(paint (flip-vert einstein))
+(paint (flip-horiz einstein))
