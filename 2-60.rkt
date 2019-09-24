c4d131a5850ec7e6913b7833350a9b66078daf42
Michal Darda <mdarda@pm.me>
2019-09-24 22:55:58 +0200

    2-60

---
+++ 2-60.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2))
+         '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1)
+                                 set2)))
+        (else (intersection-set (cdr set1)
+                                set2))))
+
+(define set1 (list 1 2 3))
+(define set2 (list 3 2 6 7 8))
