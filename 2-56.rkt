cf074eee868d158d53136d69066f88071efbc474
Michal Darda <mdarda@pm.me>
2019-09-24 11:26:38 +0200

    2-56

--- 2-56.rkt
+++ 2-56.rkt
@@ -6,19 +6,47 @@
        (variable? v2)
        (eq? v1 v2)))
 
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
 (define (make-sum a1 a2)
-  (list '+ a1 a2))
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 (define (addent s) (cadr s))
 (define (augend s) (caddr s))
 
 (define (make-product m1 m2)
-  (list '* m1 m2))
+  (cond ((or (=number? m1 0)
+             (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2))
+         (* m1 m2))
+        (else (list '* m1 m2))))
+
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 (define (multiplier p) (cadr p))
 (define (multiplicand p) (caddr p))
+
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e))
+         (expt b e))
+        (else (list '** b e))))
+
+(define (exponentiation? exp)
+  (and (pair? exp) (eq? (car exp) '**)))
+
+(define (base exp) (cadr exp))
+(define (exponent exp) (caddr exp))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -35,6 +63,12 @@
           (make-product
            (deriv (multiplier exp) var)
            (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (exponent exp)
+          (make-exponentiation (base exp) (- (exponent exp) 1))))
         (else (error "unknown expression type: DERIV" exp))))
 
 (deriv '(* (* x y) (+ x 3)) 'x)
+(deriv '(** x 2) 'x)
+(deriv '(** x 3) 'x)
