50a1205dc7c31ad4bc292cd43fc411697f8397a1
Michal Darda <mdarda@pm.me>
2019-10-21 22:42:05 +0200

    3-05

---
+++ 3-05.scm
@@ -0,0 +1,48 @@
+(import (chicken random))
+
+(define random pseudo-random-integer)
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (rand)
+  (random 10000))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else (iter (- trials-remaining 1)
+                      trials-passed))))
+  (iter trials 0))
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(estimate-pi 1000)
+
+(define (estimate-integral P x1 x2 y1 y2 trials)
+  (define (test)
+    (P (random-in-range x1 x2) (random-in-range y1 y2)))
+  (monte-carlo trials test))
+
+(define (P x y)
+  (<= (+ (square (- x 5)) (square (- y 7)))
+      (square 3)))
+
+(estimate-integral P 2 8 4 10 1000)
+
+(define (estimate-pi)
+  (define (circle x y)
+    (<= (+ (* x x) (* y y)) 1))
+  (* (estimate-integral circle -1 1 -1 1 100000)
+     4.0))
+
+(estimate-pi)
