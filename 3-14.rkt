388e85be95f47391d7cdb5de718a2c4e686d6488
Michal Darda <mdarda@pm.me>
2020-05-12 00:58:38 +0200

    3-14

---
+++ 3-14.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ([temp (cdr x)])
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+
+;; 1st iteration
+
+;; temp (list 'b 'c 'd)
+
+;; x =  ('a '())
+
+;; loop (list 'b 'c 'd) (list 'a '())
+
+;; temp 'c 'd
+;; x = 'b 'a '()
+
+;; loop ('c 'd) (list 'b 'a)
+
+;; so this fn reverses a list
