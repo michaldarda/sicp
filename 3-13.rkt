4b407281cb6dc3fb4b944d082ef01d27e15d34b4
Michal Darda <mdarda@pm.me>
2020-05-12 00:46:23 +0200

    3-12, 3-13

---
+++ 3-13.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+
+;; (last-pair z)
+
+;; ^ causes stack overflow cause this
+;; list is cycle
