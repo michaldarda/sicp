158c102d55d67f75acbd33f6f059cb68f8501664
Michal Darda <mdarda@pm.me>
2020-05-17 23:38:51 +0200

    Add custom comparator

--- 3-26.rkt
+++ 3-26.rkt
@@ -18,31 +18,34 @@
 (define (make-tree entry left right)
   (list entry left right))
 
-(define (lookup-key x set)
-  (cond ((null? set) false)
-        ((= x (key (entry set))) (entry set))
-        ((< x (key (entry set)))
-         (lookup-key
-          x
-          (left-branch set)))
-        ((> x (key (entry set)))
-         (lookup-key
-          x
-          (right-branch set)))))
+(define (lookup-key k tree)
+  (if (null? tree)
+      false
+      (let ([comparison-result (compare k (key (entry tree)))])
+        (cond [(eq? comparison-result 0)
+               (entry tree)]
+              [(eq? comparison-result 1)
+               (lookup-key
+                k
+                (left-branch tree))]
+              [(eq? comparison-result -1)
+               (lookup-key
+                k
+                (right-branch tree))]))))
 
-(define (adjoin-set k v set)
-  (cond ((null? set) (make-tree (make-entry k v) '() '()))
-        ((= k (key (entry set))) set)
-        ((< k (key (entry set)))
-         (make-tree
-          (entry set)
-          (adjoin-set k value (left-branch set))
-          (right-branch set)))
-        ((> k (key (entry set)))
-         (make-tree
-          (entry set)
-          (left-branch set)
-          (adjoin-set k value (right-branch set))))))
+(define (insert-tree k v tree)
+  (if (null? tree)
+      (make-tree (make-entry k v) '() '())
+      (let ([comparison-result (compare k (key (entry tree)))])
+        (cond [(eq? comparison-result 0) tree]
+              [(eq? comparison-result 1)
+               (make-tree
+                (entry tree)
+                (insert-tree k value (left-branch tree)))]
+              [(eq? comparison-result -1)
+               (make-tree
+                (entry tree)
+                (insert-tree k value (right-branch tree)))]))))
 
 (define (lookup key table)
   (let ([record (lookup-key key (cdr table))])
@@ -54,13 +57,20 @@
   (let ([record (lookup-key key (cdr table))])
     (if record
         (set-value! record value)
-        (set-cdr! table (adjoin-set key value (cdr table)))))
+        (set-cdr! table (insert-tree key value (cdr table)))))
   'ok)
 
 (define (make-table)
   (list '*table*))
 
+(define (compare char-a char-b)
+  (let ([charcode-a (char->integer char-a)]
+        [charcode-b (char->integer char-b)])
+    (cond [(< charcode-a charcode-b) 1]
+          [(> charcode-a charcode-b) -1]
+          [else 0])))
+
 (define x (make-table))
 
-(insert! 1 'a x)
-(lookup 1 x)
+(insert! #\A 1 x)
+(lookup #\A x)
